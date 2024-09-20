eaf41de72ea6bfb690ea32521155769e2b4d3f86
Dabom Ahn <900428star@naver.com>
2024-09-20 11:06:28 +0900

    속성프로퍼티 적용시 주의사항

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ btn.addEventListener("click", () => {
   //속성값 설정시 px단위는 숫자로만 입력
   //퍼센트 단위는 문자열로 감싸서 입력
   //모션시간인 duration은 숫자만입력 (ms단위) 디폴트값: 500
-  //opacity 제어: 0~1 사이의 실수값
-  //제어가능 속성: width, height, opacity, top, left, bottom, left, color, backgroundcolor, scroll
-  new Anime(box, { top: "50%", left: "50%", opacity: 0 }, { duration: 1000 });
+  //opacity 제어 : 0~1사이의 실수값
+  //제어 가능 속성: width, height, opacity, top, left, bottom, left, color, backgroundColor, scroll
+  //margin, padding도 모션 처리 가능 (단 여백관련 속성은 %값 적용 불가, px단위로만 변경 가능)
+  //속성명은 camel case 규칙 적용하거나 아니면 ''로 감싸줌 (예: margin-left는 marginLeft 혹은 'margin-left'식으로 사용 )
+  new Anime(box, { "padding-left": 300 }, { duration: 1000 });
 });
-
 // 괄호 안에 제어 대상 이름 넣기 
